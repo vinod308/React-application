c7644a785f00186f9852c7e505158ca3d5075ba7
vinod308 <vinod.kumar@iic.ac.in>
2020-09-16 20:00:40 +0530

    Updates

---
+++ precache-manifest.d0c3c44cf6086e19f1313146c6c06029.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e5b84895543a493bbab4ef25033b23",
+    "url": "/React-application/index.html"
+  },
+  {
+    "revision": "2179f94e988d70851218",
+    "url": "/React-application/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "65c89b511cbc5d366dff",
+    "url": "/React-application/static/css/main.592793f1.chunk.css"
+  },
+  {
+    "revision": "2179f94e988d70851218",
+    "url": "/React-application/static/js/2.b286d6a8.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-application/static/js/2.b286d6a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c89b511cbc5d366dff",
+    "url": "/React-application/static/js/main.98be6d80.chunk.js"
+  },
+  {
+    "revision": "3aaf281652c69cad3b3e",
+    "url": "/React-application/static/js/runtime-main.c1e0faec.js"
+  },
+  {
+    "revision": "94b6feea48c06dd8d1ce15a0b952ed65",
+    "url": "/React-application/static/media/img4.94b6feea.jpg"
+  },
+  {
+    "revision": "87356bb1ad4308a7d4639a267da6f13a",
+    "url": "/React-application/static/media/img5.87356bb1.jpg"
+  },
+  {
+    "revision": "f677f4fe782bc13717c8df99aae008f8",
+    "url": "/React-application/static/media/img6.f677f4fe.jpg"
+  },
+  {
+    "revision": "4aefeea9954987cfc3b9f344a3cc154c",
+    "url": "/React-application/static/media/img7.4aefeea9.jpg"
+  },
+  {
+    "revision": "e6b8bc5b72d27e72c748555dfa08546f",
+    "url": "/React-application/static/media/img8.e6b8bc5b.jpg"
+  },
+  {
+    "revision": "81889773690ddbfbb17805302a65a02b",
+    "url": "/React-application/static/media/img9.81889773.jpg"
+  },
+  {
+    "revision": "b33ebee0e20ee3aab161821608336733",
+    "url": "/React-application/static/media/react2.b33ebee0.jpg"
+  },
+  {
+    "revision": "5001540476f6b71a5fd52c966241f072",
+    "url": "/React-application/static/media/react3.50015404.png"
+  }
+]);
